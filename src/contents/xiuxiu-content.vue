3a5051432c20c9188a9721c4c770ddf031c2ae8d
GXG <936806643@qq.com>
2023-12-10 19:10:22 +0800

    perf:

--- src/contents/xiuxiu-content.vue
+++ src/contents/xiuxiu-content.vue
@@ -1,21 +1,50 @@
 <template>
   <div>
-    <button @click="changeCharacter" title="(“”‘’：，；;)">替换中英文符号</button>
-    <button @click="clg">clg</button>
+    <button @click="changeCharacter" title="(“”‘’：，；;)斜体">替换中英文符号\斜体</button>
+    <span v-if="hasEM">所有斜体：{{allKeywords}}</span>
   </div>
 </template>
 <script setup lang="ts">
-import { reactive, ref } from "vue"
+import { reactive, ref, onMounted } from "vue"
 import type { App } from "vue"
 import type { PlasmoCSConfig } from "plasmo"
 // import popup from "~popup.vue";
 import popup from "~popup.vue";
-function clg() {
-  console.log(111,popup);
 
-}
 const config: PlasmoCSConfig = {
   matches: ["*://xiumi.us/studio/*"],
+}
+const hasEM = ref(false)
+const allKeywords = ref([])
+const findEM = (element) => {
+  let emKeywords = ""
+  let emAll = element.querySelectorAll("em")
+  if (emAll.length > 0) {
+    let keywords = []
+    hasEM.value = true
+    for (let i = 0; i < emAll.length; i++) {
+      keywords.push(emAll[i].textContent)
+    }
+    // 去重
+    keywords = Array.from(new Set(keywords))
+    allKeywords.value.push(...keywords)
+    allKeywords.value= Array.from(new Set(allKeywords.value))
+    emKeywords = keywords.join(",")
+  }
+  return emKeywords
+}
+
+function exchangeKeywords(emKeywords: string, newP: string) {
+  const keywordsArr = emKeywords.split(",");
+  let includeskeywordsNewP = newP;
+  for (let j = 0; j < keywordsArr.length; j++) {
+    if (newP.includes(keywordsArr[j])) {
+      const arr = keywordsArr[j];
+      const arrRegex = new RegExp(arr, "g");
+      includeskeywordsNewP = includeskeywordsNewP.replace(arrRegex, `<em>${arr}</em>`);
+    }
+  }
+  return includeskeywordsNewP;
 }
 function changeCharacter() {
   // const reg = /[u4e00-u9fa5]/
@@ -42,9 +71,9 @@ function changeCharacter() {
       // 判断字符串是否全是英文
       for (let i = 0; i < allP.childNodes.length; i++) {
         let p = allP.childNodes[i]
-        if (p.innerText && p.innerText !== '\n' && p.innerText !== ' ') {
-          let isEnglish = isAllEnglish(p.innerText)
-          // let isChinese = isAllChinese(p.innerText)
+        if (p.textContent && p.textContent !== '\n' && p.textContent !== ' ') {
+          let isEnglish = isAllEnglish(p.textContent)
+          // let isChinese = isAllChinese(p.textContent)
           console.log("isEnglish:", isEnglish);
           const firstP = allP.childNodes[0]
           const secondeP = allP.childNodes[1]
@@ -52,7 +81,7 @@ function changeCharacter() {
           let cnColorDefault = secondeP?.childNodes[0]?.nodeName === 'SPAN' ? secondeP.childNodes[0].style.color : '#000000'
 
           if (isEnglish) {
-            let newP = p.innerText
+            let newP = p.textContent
               .replace(/“/g, '"')
               .replace(/”/g, '"')
               .replace(/‘/g, "'")
@@ -64,12 +93,17 @@ function changeCharacter() {
             let newSpan = document.createElement('span')
             newNode.style.textAlign = 'left'
             newSpan.style.color = enColorDefault
-            newSpan.textContent = newP
+            // 斜体处理
+            let keywords = findEM(p)
+            if (hasEM.value) {
+              newP = exchangeKeywords(keywords,newP)
+            }
+            newSpan.innerHTML = newP
             newNode.appendChild(newSpan)
             allP.replaceChild(newNode, p)
           }
           else {
-            let newP1 = p.innerText
+            let newP1 = p.textContent
               .replace(/"/g, '“')
               .replace(/"/g, '”')
               .replace(/'/g, '‘')
@@ -81,7 +115,12 @@ function changeCharacter() {
             let newSpan1 = document.createElement('span')
             newNode1.style.textAlign = 'left'
             newSpan1.style.color = cnColorDefault
-            newSpan1.textContent = newP1
+            // 斜体处理
+            let keywords = findEM(p)
+            if (hasEM.value) {
+              newP1 = exchangeKeywords(keywords,newP1)
+            }
+            newSpan1.innerHTML = newP1
             newNode1.appendChild(newSpan1)
             allP.replaceChild(newNode1, p)
           }
